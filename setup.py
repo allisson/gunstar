63306237f5ccf8df49491b31ba4c2e3b83d52636
Allisson Azevedo <allisson@gmail.com>
2013-08-15 14:26:27 -0300

    Added importlib if python < 2.7.

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+import sys
+
+
+requires = [
+    'WebOb>=1.2.3',
+    'blinker>=1.3',
+    'Jinja2>=2.7.1',
+]
+if sys.version_info < (2, 7):
+    requires.append('importlib>=1.0.2')
 
 
 setup(
@@ -8,15 +18,10 @@ setup(
     author='Allisson Azevedo',
     author_email='allisson@gmail.com',
     packages=['gunstar'],
-    license='LICENSE',
+    license='MIT',
     description='Another python web framework.',
     long_description=open('README.rst').read(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'WebOb>=1.2.3',
-        'Routes>=1.13',
-        'blinker>=1.3',
-        'Jinja2>=2.7.1',
-    ]
+    install_requires=requires
 )
